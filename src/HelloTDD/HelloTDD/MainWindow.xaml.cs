44f62c496e3d58bafa826c6a491e092dbe5dbd0e
Erik Öjebo <erik.ojebo@gmail.com>
2013-01-25 12:33:23 +0100

    Implemented a super simple calculator GUI

---
+++ src/HelloTDD/HelloTDD/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace HelloTDD
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddClicked(object sender, RoutedEventArgs e)
+        {
+            var result = int.Parse(FirstNumberTextBox.Text) + int.Parse(SecondNumberTextBox.Text);
+
+            ResultTextBox.Text = result.ToString();
+        }
+
+        private void OnSubtractClicked(object sender, RoutedEventArgs e)
+        {
+            var result = int.Parse(FirstNumberTextBox.Text) - int.Parse(SecondNumberTextBox.Text);
+
+            ResultTextBox.Text = result.ToString();
+        }
+
+        private void OnMultiplyClicked(object sender, RoutedEventArgs e)
+        {
+            var result = int.Parse(FirstNumberTextBox.Text) * int.Parse(SecondNumberTextBox.Text);
+
+            ResultTextBox.Text = result.ToString();
+        }
+
+        private void OnDivideClicked(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
